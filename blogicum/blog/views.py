5f571fd52411fd016adbed6d4598eacf68ee4772
Julia Klesova <your.little.bastard90@gmail.com>
2024-03-22 17:13:52 +0300

    правки проекта после второго ревью: 1. исправлены шаблоны в папке templates - category, detail. В detail.html теперь включен шаблон post_info, добавлен фильтр linebreaksbr, убраны пустые строки 2. исправлен шаблон post_info.html в папке includes - убраны лишние строчки

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from django.http import Http404
 
 
-posts = {
-    0: {
+posts = [
+     {
         'id': 0,
         'location': 'Остров отчаянья',
         'date': '30 сентября 1659 года',
@@ -15,8 +15,8 @@ posts = {
                 несчастный Робинзон Крузо, был выброшен
                 полумёртвым на берег этого проклятого острова,
                 который назвал островом Отчаяния.''',
-    },
-    1: {
+     },
+     {
         'id': 1,
         'location': 'Остров отчаянья',
         'date': '1 октября 1659 года',
@@ -31,8 +31,8 @@ posts = {
                 непременно спаслись бы. Теперь из его обломков мы могли бы
                 построить баркас, на котором и выбрались бы из этого
                 гиблого места.''',
-    },
-    2: {
+     },
+     {
         'id': 2,
         'location': 'Остров отчаянья',
         'date': '25 октября 1659 года',
@@ -43,23 +43,31 @@ posts = {
                 жалкие обломки,  да и те видны только во время отлива.
                 Весь этот день я хлопотал  около вещей: укрывал и
                 укутывал их, чтобы не испортились от дождя.''',
-    },
-}
+     },
+]
 
 # на уровне модуля (т.к. то что расположено на уровне модуля будет просчитано
 # только один раз при запуске сервера) собираем новую структуру данных для
 # хранения постов (удобнее всего словарь), в которой можно будет за О(1)
 # получать нужный пост сразу по его id
 
-if not posts:
-    raise Http404('Указан неверный id')
+# Сделаю сначала словарь формата "id-text" (dict comprehension) генератором словарей
+# https://www.sravni.ru/kursy/info/kak-vzyat-znachenie-iz-slovarya-python-i-ne-slomat-prilozhenie/
+# https://ru.stackoverflow.com/questions/1427432/%D0%9A%D0%B0%D0%BA-%D0%BD%D0%B0%D0%B9%D1%82%D0%B8-%D0%BD%D1%83%D0%B6%D0%BD%D1%8B%D0%B9-%D1%81%D0%BB%D0%BE%D0%B2%D0%B0%D1%80%D1%8C-%D0%B5%D1%81%D0%BB%D0%B8-%D0%B5%D1%81%D1%82%D1%8C-value
+# https://smartiqa.ru/python-workbook/dict#2
 
+posts_dict = {}
+for item in posts:
+    posts_dict.update(item)
+if not posts_dict:
+    raise Http404('Указан неверный id')
 
 
 def index(request):
     """Вывод записи на главную страницу."""
+    sorted_dict = dict(sorted(posts_dict.items()))
     template_name = 'blog/index.html'
-    context = {'posts_list': posts[::-1]}
+    context = {'posts_dict': posts_dict}                   #dict(sorted(posts_dict.items()))}    # {'posts_list': posts[::-1]}
     return render(request, template_name, context)
 
 
